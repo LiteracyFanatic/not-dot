d4fc437d133ba540e67a2ba6608b5429242476db
Jordan Cannon <LiteracyFanatic@gmail.com>
2021-01-28 08:42:31 -0600

    Update config.py for qutebrowser v2.0.0

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,3 +1,4 @@
+config.load_autoconfig(False)
 c.content.notifications = False
 c.editor.command = ['xfce4-terminal', '-x', 'nvim', '{file}']
 c.scrolling.bar = 'always'
@@ -17,9 +18,16 @@ config.bind(',n', 'spawn -v nh {url}')
 c.auto_save.session = True
 c.input.partial_timeout = 10000
 c.messages.timeout = 5000
-c.tabs.background = True
 c.tabs.last_close = 'close'
 c.tabs.mousewheel_switching = False
 c.downloads.position = 'bottom'
 c.aliases = { "readability": "spawn --userscript readability" }
 c.content.user_stylesheets = ['custom.css']
+c.content.ssl_strict = False
+c.content.blocking.method = 'adblock'
+c.content.blocking.adblock.lists = [
+    'https://easylist-downloads.adblockplus.org/easylist.txt',
+    'https://easylist-downloads.adblockplus.org/abp-filters-anti-cv.txt',
+    'https://easylist-downloads.adblockplus.org/fanboy-annoyance.txt'
+]
+c.changelog_after_upgrade = 'patch'
