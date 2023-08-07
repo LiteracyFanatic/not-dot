c14e63debba51384ea2c60ef747cfa26056bf11d
Jordan Cannon <LiteracyFanatic@gmail.com>
2023-08-07 10:54:40 -0500

    Use oh-my-posh for prompt instead of pureline
    oh-my-posh supports powershell as well as bash

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -4,3 +4,4 @@ Set-PSReadLineKeyHandler -Key Tab -Function TabCompleteNext
 Set-PSReadLineKeyHandler -Key Shift+Tab -Function TabCompletePrevious
 Set-PSReadLineKeyHandler -Key Alt+SpaceBar -Function Complete
 Set-PSReadLineOption -HistorySearchCursorMovesToEnd
+oh-my-posh init pwsh --config "$HOME/.config/oh-my-posh/theme.omp.json" | Invoke-Expression
